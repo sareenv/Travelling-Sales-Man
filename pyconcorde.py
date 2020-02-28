9f15557b8892e80b71509fadfdd7add5b8a679a5
harjaus <harjaus@coventry.ac.uk>
2020-02-28 01:11:30 +0000

    Script to compute PyConcorde solutions given Points

---
+++ pyconcorde.py
@@ -0,0 +1,31 @@
+from concorde.tsp import TSPSolver 
+from time import time
+
+pointsList=[]
+for line in open("input_points.txt", "r").readlines():
+    currentPoints = []
+    for element in line[1:-3].split("), "):
+        currentPoints.append((int(element[1:].split(",")[0]), int(element[1:].split(",")[1])))
+    pointsList.append(currentPoints)
+solutions=[]
+times=[]
+for element in pointsList[50:]:
+    print("\n\nMOVING ON TO ", len(solutions))
+    print("\n\n")
+    solver = TSPSolver.from_data(*zip(*element), "EUC_2D")
+    start = time()
+    solution = solver.solve()
+    while not solution.found_tour:
+        pass #wait until we find tour
+    totalTime = time()-start
+    solutions.append(solution.tour)
+    times.append(totalTime)
+
+output = open("output_solutions.txt","w")
+timeOut = open("output_time.txt", "w")
+for sol, time in zip(solutions, times):
+    outputStream = "["
+    for element in sol:
+        outputStream += str(element) + ", "
+    output.write(outputStream[:-2] + "]\n")
+    timeOut.write(str(time) + "\n")
